d0aa43f45c1883fb85ccdb65f8a48f4dc27395e6
Anthony Jonas <anthony.jonas@outlook.com>
2019-01-16 14:43:47 +0100

    création service pour récupérer films populaires sur l'API

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,23 @@
 
 namespace App\Controller;
 
+use App\Service\MovieLister;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param MovieLister $movieLister
+     * @return Response
      */
-    public function index()
+    public function index(MovieLister $movieLister) :Response
     {
-        return $this->render('home/index.html.twig');
+        $popularMovies = $movieLister->listPopularMovies();
+        return $this->render('home/index.html.twig', [
+            'popularMovies' => $popularMovies,
+        ]);
     }
 }
--- src/Service/MovieLister.php
+++ src/Service/MovieLister.php
@@ -9,21 +9,17 @@
 namespace App\Service;
 
 use GuzzleHttp;
-use GuzzleHttp\Exception\GuzzleException;
 
 class MovieLister
 {
     const API_KEY = '97cf4d88a44535593e484e35ebfd342f';
 
-    public function listLastMoviesAdded()
+    public function listPopularMovies()
     {
         $client = new GuzzleHttp\Client();
-        try {
-            $request = $client->request('GET', 'https://api.themoviedb.org/3/search/movie/lastest?api_key=' . self::API_KEY . '&language=fr-FR&region=FR');
-        } catch (GuzzleException $e) {
-        }
-        $lastMovies = json_decode($request->getBody(), true);
+        $request = $client->request('GET', 'https://api.themoviedb.org/3/movie/popular?api_key=' . self::API_KEY . '&language=fr-FR&region=FR');
+        $popularMovies = json_decode($request->getBody(), true);
 
-        return $lastMovies;
+        return $popularMovies;
     }
 }
