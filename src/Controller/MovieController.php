bafe45749e780f0db06c708ba0704bdbfd6ce863
Anthony Jonas <anthony.jonas@outlook.com>
2019-01-21 10:29:10 +0100

    création méthode pour lister les films par Id

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Service\MovieLister;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
     /**
-     * @Route("/movie", name="movie")
+     * @Route("/movie", name="movie_index")
      */
     public function index()
     {
         return $this->render('movie/index.html.twig', [
             'controller_name' => 'MovieController',
         ]);
+    }
+
+    /**
+     * @Route("/movie/{id}", name="movie_show")
+     * @param MovieLister $movieLister
+     * @return Response
+     */
+    public function show(MovieLister $movieLister) :Response
+    {
+        $movieDetails = $movieLister->listOneMovieById()
+        return $this->render('movie/show.html.twig');
     }
 }
